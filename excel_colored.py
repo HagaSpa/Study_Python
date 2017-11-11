fbe608e68af5e0c57241397469373772880d703f
HagaSpa <justicesparrow13@gmail.com>
2017-11-11 12:20:57 +0900

    【※要リファクタリング】別関数からも変数にアクセスできるように、グローバル変数追加。 （ classを作るべき？ベターがわからん・・・）

--- excel_colored.py
+++ excel_colored.py
@@ -7,6 +7,11 @@ import openpyxl as px
 excel_colored(fileName, color, target)
 """
 
+#  グローバル変数定義
+file_name = ""
+color = ""
+target = ""
+
 # 引数をパースする
 def parser():
     args = sys.argv
@@ -17,13 +22,22 @@ def parser():
     # 引数として自動的に付与される、ファイル名自身を削除
     args.pop(0)
     # 引数を変数へ代入
+    global file_name
     file_name = args[0]
+    global color
     color = args[1]
+    global target
     target = args[2]
     return True
+
+# 色付けを実行する
+def colored():
+    global file_name
+    print (file_name)
+
 
 # main処理
 if __name__ == '__main__':
     parse_result = parser()
     if (parse_result):
-        print ("ok")
+        colored()
